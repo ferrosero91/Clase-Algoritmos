3498fd8863b720de7cecda9e009e8b2a93f99e12
ferrosero <ferrosero91@gmail.com>
2024-02-29 12:42:39 -0500

    guardar

--- Triangulo/Color.py
+++ Triangulo/Color.py
@@ -2,3 +2,17 @@ class Color:
     r=0
     g=0
     b=0
+
+
+#Metodos
+
+def consultarR(self):
+    return self.r
+
+
+def consultarG(self):
+    return self.g
+
+
+def consultarB(self):
+    return self.b
--- Triangulo/Punto.py
+++ Triangulo/Punto.py
@@ -1,3 +1,16 @@
 class Punto:
     x=0
     y=0
+    
+#Metodos
+
+def ConsultarCordenadaX(self):
+    return self.x
+
+def ConsultarCordenadaY(self):
+    return self.y
+
+
+
+
+    
